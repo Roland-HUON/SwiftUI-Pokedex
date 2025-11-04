b3fcd89b380f00d14dfb4bc8498456d3d1cc4030
Roland HUON <huonroland91@gmail.com>
2025-11-04 08:39:39 +0100

    Initial Commit

---
+++ SwiftUI-Pokedex/SwiftUI_PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PokedexApp.swift
+//  SwiftUI-Pokedex
+//
+//  Created by HUON Roland on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-PokedexTests/SwiftUI_PokedexTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUI_PokedexTests.swift
+//  SwiftUI-PokedexTests
+//
+//  Created by HUON Roland on 04/11/2025.
+//
+
+import Testing
+
+struct SwiftUI_PokedexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
