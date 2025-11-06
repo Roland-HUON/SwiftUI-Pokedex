7afeb90cefa4510fcc8eddc216ffcae3be5b07b2
Roland HUON <huonroland91@gmail.com>
2025-11-06 13:20:02 +0100

    update UserMock

--- SwiftUI-Pokedex/Core/Networks/Users/UserMock.swift
+++ SwiftUI-Pokedex/Core/Networks/Users/UserMock.swift
@@ -12,35 +12,58 @@ final class UserMock {
     
     private init() {}
     
-    func getFriends() -> [UserModel] {
+    func getUsers() -> [UserModel] {
         return [
             UserModel(
                 id: 1,
-                pseudo: "Lulu",
-                firstname: "Lucie",
-                lastName: "Martin",
-                avatar: "https://randomuser.me/api/portraits/women/1.jpg",
-                idLogin: "lucie01",
+                pseudo: "Iris",
+                firstname: "Iris",
+                lastName: "Unknown",
+                avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3l3Ss4ciBkVzmFw7TUZoUGlsL8vZQzIRU5A&s",
+                idLogin: "IrisH",
                 password: "password"
             ),
             UserModel(
                 id: 2,
-                pseudo: "Tomy",
-                firstname: "Thomas",
-                lastName: "Durand",
-                avatar: "https://randomuser.me/api/portraits/men/2.jpg",
-                idLogin: "thomasD",
+                pseudo: "Serena",
+                firstname: "Serena",
+                lastName: "Unknown",
+                avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4MUJSdbbzwUHnh62-JLcgtQk9fbs3PloNIg&s",
+                idLogin: "Serena2000",
                 password: "123456"
             ),
             UserModel(
                 id: 3,
-                pseudo: "Juju",
-                firstname: "Julien",
-                lastName: "Bernard",
-                avatar: "https://randomuser.me/api/portraits/men/3.jpg",
-                idLogin: "jujuB",
+                pseudo: "Cynthia",
+                firstname: "Cynthia",
+                lastName: "Unknown",
+                avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe3NDfSHhg_bE8dvDES34ZxEYJSQ_XlU8iRA&s",
+                idLogin: "Cynthia123",
                 password: "azerty"
-            )
+            ),
+            UserModel(
+                id: 4,
+                pseudo: "Pikapika",
+                firstname: "Roland",
+                lastName: "HUON",
+                avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEotLJYD-lr06mEXAJr4S4B8GwWX_Ix_ZjaQ&s",
+                idLogin: "pikapika",
+                password: "pikachuchu"
+            ),
+            UserModel(
+                id: 5,
+                pseudo: "Pierre",
+                firstname: "Pierre",
+                lastName: "Unknown",
+                avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUdiS8DrsqwB-cKiqfcfUHIKu9YVCuzpC4O7LfRDHMO4IJIfHuVmN4iMJvjSpgoe263cE&usqp=CAU",
+                idLogin: "Pierre36",
+                password: "pierre"
+            ),
         ]
     }
+    
+    static func validate(id: String, password: String) -> UserModel? {
+        return shared.getUsers().first { $0.idLogin == id && $0.password == password }
+    }
+    
 }
